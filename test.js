233f1ce1f9031da189c72ff77bd51693258ced48
Trygve Lie <post@trygve-lie.com>
2019-12-02 16:07:03 +0100

    Feature: Added new bar() method

--- test.js
+++ test.js
@@ -3,7 +3,13 @@
 const { test } = require('tap');
 const index = require('./index');
 
-test('Index() - test', (t) => {
+test('Index() - foo', (t) => {
     t.equal(index.foo(), 42, '.value should be 42');
+    t.end();
+});
+
+
+test('Index() - bar', (t) => {
+    t.equal(index.bar(), 'beer', '.value should be "beer"');
     t.end();
 });
